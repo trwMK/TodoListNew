bf127e0735d093bf41914f11316af8be239e39f4
Mike Koenders <65424169+trwMK@users.noreply.github.com>
2023-09-13 00:47:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
+import TodoList from "./TodoList";
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  function addTodo(title) {
+    setTodos((currentTodos) => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title, completed: false }
+    ]);
+  }
+
+  return (
+    <>
+      <NewTodoForm addTodo={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  );
+}
